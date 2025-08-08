9f7a9329cbdecb94ad7d681274fa690f57629d12
akzj <fu.niukey@gmail.com>
2025-08-08 10:48:49 +0800

    add proto

---
+++ build.rs
@@ -0,0 +1,10 @@
+// build.rs
+use tonic_prost_build::configure;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    configure()
+        .type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]")
+        .compile_protos(&["proto/pb.proto"], &["proto/"])?;
+
+    Ok(())
+}
